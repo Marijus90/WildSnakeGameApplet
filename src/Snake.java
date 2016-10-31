b7adae1c45b3c526937ea31352412101d12c38f0
Marijus <Marijus>
2016-10-31 18:19:56 +0000

    first buggy version of WildSnake game

---
+++ src/Snake.java
@@ -0,0 +1,87 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Snake {
+	
+	private List<Point> snakePoints;
+	private int xDirectionOfMovement, yDirectionOfMovement;
+	boolean isMoving, elongate;
+	private final int INITIAL_SNAKE_LENGTH = 10;
+	private final int SIZE_OF_SNAKES_POINT = 8;
+	private final int START_POSITION_X = 290;
+	private final int START_POSITION_Y = 290;
+	
+	public Snake() {
+		snakePoints = new ArrayList<Point>();
+		xDirectionOfMovement = 0;
+		yDirectionOfMovement = 0;
+		isMoving = false;
+		elongate = false;
+		
+		//Initialising the snake
+		snakePoints.add(new Point (START_POSITION_X, START_POSITION_Y));
+		for(int i = 0; i < INITIAL_SNAKE_LENGTH; i++) {
+			snakePoints.add(new Point(START_POSITION_X - i * SIZE_OF_SNAKES_POINT, START_POSITION_Y));
+		}
+	
+	}
+	
+	public void draw (Graphics g) {
+		//Colour of the snake
+		g.setColor(Color.GREEN);
+		
+		for(Point p : snakePoints) {
+			g.fillRect(p.getX(), p.getY(), SIZE_OF_SNAKES_POINT, SIZE_OF_SNAKES_POINT);
+		}
+	}
+	
+	public void move() {
+		if(isMoving) {
+			Point currentPositionOfHead = snakePoints.get(0);
+			Point currentPositionOfLastPoint = snakePoints.get(snakePoints.size() - 1);
+			//Move the head
+			Point newPositionOfHead = new Point(currentPositionOfHead.getX() + xDirectionOfMovement * SIZE_OF_SNAKES_POINT,
+					currentPositionOfLastPoint.getY() + yDirectionOfMovement * SIZE_OF_SNAKES_POINT);
+			snakePoints.set(0, newPositionOfHead);
+			//Move the body
+			for(int i = snakePoints.size() - 1; i >= 1; i--) {
+				snakePoints.set(i, snakePoints.get(i -1 ));
+			}
+		}
+
+	}
+	
+	public int getXDirectionOfMovement() {
+		return xDirectionOfMovement;
+	}
+	
+	public int getYDirectionOfMovement() {
+		return yDirectionOfMovement;
+	}
+	
+	public void setXDirectionOfMovement(int x) {
+		xDirectionOfMovement = x;
+	}
+	
+	public void setYDirectionOfMovement(int y) {
+		yDirectionOfMovement = y;
+	}
+	
+	public int getXSnakePosition() {
+		return snakePoints.get(0).getX();
+	}
+	
+	public int getYSnakePosition() {
+		return snakePoints.get(0).getY();
+	}
+	
+	public boolean getIsMoving() {
+		return isMoving;
+	}
+	
+	public void setIsMoving(boolean booleanValue) {
+		isMoving = booleanValue;
+	}
+}
---
+++ src/SnakeGame.java
@@ -0,0 +1,97 @@
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+@SuppressWarnings("serial")
+public class SnakeGame extends Applet implements Runnable, KeyListener {
+
+	public final int WINDOW_WIDTH_X = 600;
+	public final int WINDOW_HEIGHT_Y = 600;
+	
+	private Graphics gfx;
+	private Image img;
+	Thread thread;
+	Snake snake;
+
+	
+	public void init() {
+		this.resize(WINDOW_WIDTH_X, WINDOW_HEIGHT_Y);
+		img = createImage(WINDOW_WIDTH_X, WINDOW_HEIGHT_Y);
+		gfx = img.getGraphics();
+		this.addKeyListener(this);
+		snake = new Snake();
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+	public void paint(Graphics g) {
+		//Background colour
+		gfx.setColor(Color.DARK_GRAY);
+		gfx.fillRect(0, 0, WINDOW_WIDTH_X, WINDOW_HEIGHT_Y);
+		snake.draw(gfx);
+		
+		g.drawImage(img, 0, 0, null);
+	}
+	
+	
+	public void repaint(Graphics g) {
+		paint(g);
+	}
+	
+	public void update(Graphics g) {
+		paint(g);
+	}
+	
+	public void run() {
+		for(;;) {
+			
+			snake.move();
+			this.repaint();
+			try { Thread.sleep(40); } catch (InterruptedException e) { e.printStackTrace(); }
+		}
+	}
+
+	public void keyPressed(KeyEvent e) {
+		snake.setIsMoving(true);
+		switch (e.getKeyCode()) { 
+			case KeyEvent.VK_UP:
+				if(snake.getYDirectionOfMovement() != 1) {
+					snake.setYDirectionOfMovement(-1);
+					snake.setXDirectionOfMovement(0);
+					break;
+				}
+			case KeyEvent.VK_DOWN:
+				if(snake.getYDirectionOfMovement() != -1) {
+					snake.setYDirectionOfMovement(1);
+					snake.setXDirectionOfMovement(0);
+					break;
+				}
+			case KeyEvent.VK_LEFT:
+				if(snake.getXDirectionOfMovement() != 1) {
+					snake.setXDirectionOfMovement(-1);
+					snake.setYDirectionOfMovement(0);
+					break;
+				}
+			case KeyEvent.VK_RIGHT:
+				if(snake.getXDirectionOfMovement() != 1) {
+					snake.setXDirectionOfMovement(-1);
+					snake.setYDirectionOfMovement(0);
+					break;
+				}
+		}
+	}
+
+	public void keyReleased(KeyEvent arg0) {
+		
+	}
+
+	public void keyTyped(KeyEvent arg0) {
+		
+	}
+	
+	
+	
+}
